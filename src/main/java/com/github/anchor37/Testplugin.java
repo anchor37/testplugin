1f007966ae78e9dd39782a2abc9e25152b040229
たまさん <anchor370@gmail.com>
2016-05-04 16:29:20 +0900

    init

---
+++ src/main/java/com/github/anchor37/Testplugin.java
@@ -0,0 +1,15 @@
+package com.github.anchor37;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Testplugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("onEnableメソッドが呼び出されたよ！！");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("onDisableメソッドが呼び出されたよ！！");
+	}
+}
